7b2244cceaff4857fc3ec1168e150a74b04fb2c3
Raviolii <33496069+Raviolii@users.noreply.github.com>
2026-04-20 17:33:52 +0200

    Update index.ts

--- src/extractor/index.ts
+++ src/extractor/index.ts
@@ -19,6 +19,7 @@ import { Streamtape } from './Streamtape';
 import { SuperVideo } from './SuperVideo';
 import { Uqload } from './Uqload';
 import { Vidora } from './Vidora';
+import { Vidara } from './Vidara';
 import { Vidsonic } from './Vidsonic';
 import { VidSrc } from './VidSrc';
 import { VixSrc } from './VixSrc';
@@ -52,6 +53,7 @@ export const createExtractors = (fetcher: Fetcher): Extractor[] => {
     new SuperVideo(fetcher),
     new Uqload(fetcher),
     new Vidora(fetcher),
+    new Vidara(fetcher),
     new Vidsonic(fetcher),
     new VidSrc(fetcher, [ // https://vidsrc.domains/
       'vidsrcme.ru',
