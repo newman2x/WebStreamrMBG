69811adda56f5a3ed915f35723eb6a3badbb3514
Raviolii <33496069+Raviolii@users.noreply.github.com>
2026-04-23 16:12:03 +0200

    Update Vidsonic.test.ts

--- src/extractor/Vidsonic.test.ts
+++ src/extractor/Vidsonic.test.ts
@@ -2,7 +2,7 @@ import winston from 'winston';
 import { createTestContext } from '../test';
 import { FetcherMock } from '../utils';
 import { ExtractorRegistry } from './ExtractorRegistry';
-import { Vidssonic } from './Vidsonic';
+import { Vidsonic } from './Vidsonic';
 
 const logger = winston.createLogger({ transports: [new winston.transports.Console({ level: 'nope' })] });
 const extractorRegistry = new ExtractorRegistry(logger, [new Vidsonic(new FetcherMock(`${__dirname}/__fixtures__/Vidsonic`))]);
