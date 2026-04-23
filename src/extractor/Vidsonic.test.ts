e4890fadc128f4f81a66f43e1aab1e969cf07a84
Raviolii <33496069+Raviolii@users.noreply.github.com>
2026-04-23 15:51:56 +0200

    Add tests for Vidsonic extractor

---
+++ src/extractor/Vidsonic.test.ts
@@ -0,0 +1,24 @@
+import winston from 'winston';
+import { createTestContext } from '../test';
+import { FetcherMock } from '../utils';
+import { ExtractorRegistry } from './ExtractorRegistry';
+import { Vidara } from './Vidsonic';
+
+const logger = winston.createLogger({ transports: [new winston.transports.Console({ level: 'nope' })] });
+const extractorRegistry = new ExtractorRegistry(logger, [new Vidara(new FetcherMock(`${__dirname}/__fixtures__/Vidsonic`))]);
+
+const ctx = createTestContext();
+
+describe('Vidsonic', () => {
+  test('Movie ', async () => {
+    expect(await extractorRegistry.handle(ctx, new URL('https://vidsonic.net/e/vk1nj8cd2077'))).toMatchSnapshot();
+  });
+
+  test('Movie', async () => {
+    expect(await extractorRegistry.handle(ctx, new URL('https://vidsonic.net/e/8wg0n7ktvsxn'))).toMatchSnapshot();
+  });
+
+  test('Alternative domain (.so)', async () => {
+    expect(await extractorRegistry.handle(ctx, new URL('https://vidsonic.net/e/8d9dirx902q6'))).toMatchSnapshot();
+  });
+});
